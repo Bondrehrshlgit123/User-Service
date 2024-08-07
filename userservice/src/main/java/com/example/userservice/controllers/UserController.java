0cef02ad618a05ad46e2e5c737645b883ab58673
Bondrehrshlgit123 <harshalbondre123@gmail.com>
2024-08-08 03:15:53 +0530

    Added some changes in User controller

--- userservice/src/main/java/com/example/userservice/controllers/UserController.java
+++ userservice/src/main/java/com/example/userservice/controllers/UserController.java
@@ -1,15 +1,22 @@
 package com.example.userservice.controllers;
 
+import com.example.userservice.dtos.AddRolestoUserDto;
 import com.example.userservice.dtos.UserDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/users/")
 public class UserController {
     @GetMapping("/{id}")
     public UserDto getUserDetails(@PathVariable("id") Long id){
         return null;
+    }
+
+    @PostMapping("/{id}/roles")
+    public void addRolesToUser(@PathVariable("id") Long id, @RequestBody AddRolestoUserDto addRolestoUserDto){
+        List<Long> roleids=addRolestoUserDto.getRoleIds();
+
     }
 
 }
---
+++ userservice/src/main/java/com/example/userservice/services/UserService.java
@@ -0,0 +1,8 @@
+package com.example.userservice.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
